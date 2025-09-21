6f1b44808cb1718b40afaaef87d47ad530c94b82
Ayan Behlim <142768118+Ayan72978@users.noreply.github.com>
2025-09-21 23:43:37 +0530

    Add files via upload

---
+++ Mega Project 1_ Jarvis/musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "wavy": "https://www.youtube.com/watch?v=XTp5jaRU3Ws&pp=ygUEd2F2eQ%3D%3D",
+    "supreme": "https://www.youtube.com/watch?v=AX1zRInC_TA&pp=ygUHc3VwcmVtZQ%3D%3D",
+    "52 bars": "https://www.youtube.com/watch?v=4DfVxVeqk2o&pp=ygUHNTIgYmFycw%3D%3D",
+    "born to shine": "https://www.youtube.com/watch?v=dCmp56tSSmA&pp=ygUUZGlsaml0IGRvc2Fuamggc29uZ3M%3D",
+    "goat": "https://www.youtube.com/watch?v=cl0a3i2wFcc"
+} 
